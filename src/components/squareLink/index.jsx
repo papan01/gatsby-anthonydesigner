8d2c8beaed5ba9445284e7d081b9aba06a3f8222
papan01 <navy90517@gmail.com>
2020-07-16 00:49:17 +0800

    Using emotion js to solve global color

--- src/components/squareLink/index.jsx
+++ src/components/squareLink/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
+import { ScrollContext } from '../scroller';
+import SCROLL_TOP_ANIMATION from '../../constants';
 import './style.scss';
 
-function SquareLink({ children, fadeInStyle, cover, href, title, globalColor }) {
+function SquareLink({ children, fadeInStyle, cover, href, title, onMouseOver, onMouseLeave }) {
+  const [linkClassName, setLinkClassName] = useState('square-link-white');
+  const scrollContext = useContext(ScrollContext);
+  useEffect(() => {
+    if (scrollContext.scrollValue > SCROLL_TOP_ANIMATION) {
+      setLinkClassName('square-link-black ');
+    } else {
+      setLinkClassName('square-link-white');
+    }
+  }, [scrollContext.scrollValue]);
   return (
-    <div className={`square-link ${globalColor}`} style={fadeInStyle}>
-      <a href={href} title={title}>
+    <div
+      className="square-link"
+      style={fadeInStyle}
+      onMouseOver={onMouseOver}
+      onMouseLeave={onMouseLeave}
+      onFocus={onMouseOver}
+    >
+      <a href={href} title={title} className={linkClassName}>
         <Img fluid={{ ...cover.childImageSharp.fluid }} className="square-img" />
         {children}
       </a>
@@ -24,12 +41,15 @@ SquareLink.propTypes = {
   cover: PropTypes.object.isRequired,
   href: PropTypes.string.isRequired,
   title: PropTypes.string,
-  globalColor: PropTypes.string.isRequired,
+  onMouseOver: PropTypes.func,
+  onMouseLeave: PropTypes.func,
 };
 
 SquareLink.defaultProps = {
   children: null,
   title: '',
+  onMouseOver: () => {},
+  onMouseLeave: () => {},
 };
 
 export default SquareLink;
