ddb2fd0cfc5c5519aa50bacbd08fb85bb35df5e7
papan01 <navy90517@gmail.com>
2020-08-01 00:22:10 +0800

    Complete main page

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
 import React from 'react';
 import Space from '../space';
 import './style.scss';
@@ -11,6 +12,12 @@ const socialLinks = [
 ];
 
 function Footer() {
+  const clickToTop = () => {
+    if (typeof window !== 'undefined') {
+      window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    }
+  };
+
   return (
     <footer className="content">
       <Space />
@@ -34,8 +41,25 @@ function Footer() {
             ))}
           </ul>
           <Space styleClassName="space-50-100" />
+          <p className="link-murmur ">
+            Want to move forward?
+            <a href="mailto:hello@anthonydesigner.com">Drop a line.</a>
+          </p>
+        </div>
+        <div className="back-to-top-block">
+          <Space styleClassName="space-50-100" />
+          <div
+            role="button"
+            onClick={clickToTop}
+            onKeyPress={clickToTop}
+            aria-label="scroll to top"
+            className="back-to-top"
+          >
+            Back to top
+          </div>
         </div>
       </div>
+      <Space />
     </footer>
   );
 }
